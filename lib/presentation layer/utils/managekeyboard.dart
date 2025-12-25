fc0df565c3a4dc36cefc7e346f5b9fc88a0413a1
H Pc's <haiderali120705@gmail.com>
2025-12-25 15:31:23 +0500

    first commit

---
+++ lib/presentation layer/utils/managekeyboard.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: avoid_types_as_parameter_names, unused_local_variable
+
+// import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class KeyboardUtil {
+  static void hideKeyboard(BuildContext context) {
+    FocusScopeNode currentFocus = FocusScope.of(context);
+    if (!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+    }
+  }
+}
--- lib/views/profilescreen.dart
+++ lib/views/profilescreen.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 // import '../controllers/user_controller.dart';
 import '../presentation layer/controller/usercontroller.dart';
 import '../presentation layer/utils/apptheme.dart';
+import '../presentation layer/utils/managekeyboard.dart';
 // import '../core/theme/app_theme.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -30,66 +31,71 @@ void initState() {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppTheme.background,
-      appBar: AppBar(
-        title: const Text("Edit Profile"),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            /// PROFILE IMAGE
-            Obx(() {
-              final imageFile = userController.pickedImage.value;
-
-              return GestureDetector(
-                onTap: userController.pickProfileImage,
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundImage:
-                      imageFile != null ? FileImage(imageFile) : null,
-                  child: imageFile == null
-                      ? const Icon(Icons.camera_alt, size: 30)
-                      : null,
+    return GestureDetector(
+        onTap: () {
+        KeyboardUtil.hideKeyboard(context);
+      },
+      child: Scaffold(
+        backgroundColor: AppTheme.background,
+        appBar: AppBar(
+          title: const Text("Edit Profile"),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              /// PROFILE IMAGE
+              Obx(() {
+                final imageFile = userController.pickedImage.value;
+      
+                return GestureDetector(
+                  onTap: userController.pickProfileImage,
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage:
+                        imageFile != null ? FileImage(imageFile) : null,
+                    child: imageFile == null
+                        ? const Icon(Icons.camera_alt, size: 30)
+                        : null,
+                  ),
+                );
+              }),
+      
+              const SizedBox(height: 20),
+      
+              /// NAME FIELD
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  labelText: "Your Name",
                 ),
-              );
-            }),
-
-            const SizedBox(height: 20),
-
-            /// NAME FIELD
-            TextField(
-              controller: nameController,
-              decoration: const InputDecoration(
-                labelText: "Your Name",
               ),
-            ),
-
-            const SizedBox(height: 30),
-
-            /// SAVE BUTTON
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () async {
-                  final name = nameController.text;
-                  if (name.isEmpty) {
-                    Get.snackbar("Error", "Please enter your name");
-                    return;
-                  }
-                  await userController.saveUser(name);
-                  Get.back();
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: AppTheme.icon,
+      
+              const SizedBox(height: 30),
+      
+              /// SAVE BUTTON
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    final name = nameController.text;
+                    if (name.isEmpty) {
+                      Get.snackbar("Error", "Please enter your name");
+                      return;
+                    }
+                    await userController.saveUser(name);
+                    Get.back();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppTheme.icon,
+                  ),
+                  child:  Text("Save", style: TextStyle(color: Colors.white)),
                 ),
-                child:  Text("Save", style: TextStyle(color: Colors.white)),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
